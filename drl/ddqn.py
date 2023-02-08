14d884cbf4274012c480d176f974cdf5f0c695fd
lcrosato <luca.crosato@northumbria.ac.uk>
2023-02-08 18:11:05 +0100

    Added wandb support

--- drl/ddqn.py
+++ drl/ddqn.py
@@ -21,9 +21,9 @@ class DDQN:
                  learning_rate = 1e-03,
                  eps_min = 0.01,
                  eps_decay = 0.99,
-                 episodes_per_epoch = 100,
                  start_learn=100,
                  batch_size=16,
+                 episodes_per_epoch = 100,
                  network_update_frequency=1_000):
         self._memory = deque(maxlen=replay_buffer_size)
         self.gamma = gamma
@@ -46,49 +46,54 @@ class DDQN:
                                           lr=self.learning_rate)
         
     def learn(self, 
-              N_iter=int(100_000),
+              total_timesteps,
               log = True,
-              callback_fn = None):
+              epoch_callback = None,
+              episode_callback = None):
         # Perform initial training setup
         self._setup_train()
         
         # Start new epoch and first episode
+        epoch_count = 0
         self._start_new_epoch()
         state = self._start_new_episode()
         
-        # Start training iterations
-        for count in range(N_iter):
-            self._update_epsilon()      # Update e-greedy value
+        # Training main loop
+        for count in range(total_timesteps):
             
-            # Gather and store new transition
+            # Update e-greedy value
+            self._update_epsilon()     
+            
+            # Gather and store new transition in replay buffer
             action = self._eps_greedy_action_selection(state)
             new_state, reward, done, info = self.env.step(action)
             self._add_experience(state, action, reward, new_state, done)
             
-            # Reset environment if episode is finished
             if done:
-                if log:
-                    print(f"Episode reward: {self.env.episode_reward}, {info['end_reason']}")
-                state = self._start_new_episode()
-                self.epoch_reward += self.env.episode_reward
+                self._cumulative_epoch_reward += self.env.episode_reward
+                self._episode_print(info) if log else None
+                
+                # End of episode
+                if episode_callback is not None:
+                    episode_callback()
                 
                 if self._end_of_epoch():
-                    if log:
-                        self._print_reward(self.epoch_reward)
+                    # End of epoch
+                    epoch_count += 1
+                    avg_epoch_reward = self._cumulative_epoch_reward/self.episodes_per_epoch
+                    self._epoch_print(avg_epoch_reward) if log else None
+                    
+                    if epoch_callback is not None:
+                        epoch_callback(avg_epoch_reward, 
+                                       epoch_count, 
+                                       self.policy) 
+                        
                     self._start_new_epoch()
+                    
+                state = self._start_new_episode()
                 
-            if len(self._memory) > self.start_learn: 
-                # LEARN PHASE
-                self.optimizer.zero_grad()
-                minibatch = random.sample(self._memory, self.batch_size)
-                
-                loss = self._q_loss(minibatch)
-                loss.backward()
-                self.optimizer.step()
-                
-                # Update target network
-                if count % self.network_update_frequency == 0:
-                    self._update_target_policy()
+            # Update weights
+            self._update_network_weights(count)
                     
                     
                     
@@ -99,7 +104,7 @@ class DDQN:
         self._memory.append((old_state, action, reward, new_state, done))
         
     def _update_target_policy(self):
-        self.target_policy.set_weights(self.policy.get_weights())
+        self.target_policy.load_state_dict(self.policy.state_dict())
         
     def _eps_greedy_action_selection(self, current_state):
         with torch.no_grad():
@@ -117,9 +122,12 @@ class DDQN:
         self.epsilon *= self.epsilon_decay
         self.epsilon = max(self.epsilon, self.epsilon_min)
         
-    def _print_reward(self, epoch_reward):                    
-        print(f"\tTRAIN: Cumulative reward: {epoch_reward/self.episodes_per_epoch}")
-        
+    def _epoch_print(self, avg_epoch_reward):                    
+        print(f"\tTRAIN: {self.episodes_per_epoch} episodes average reward: {avg_epoch_reward}")
+    
+    def _episode_print(self, info):                    
+        print(f"Episode reward: {self.env.episode_reward}, {info['end_reason']}")
+
     def _end_of_epoch(self):
         return (self._episode_count % self.episodes_per_epoch) == 0
                 
@@ -134,7 +142,7 @@ class DDQN:
     
     def _start_new_epoch(self):
         self._episode_count = 0
-        self.epoch_reward = 0.0
+        self._cumulative_epoch_reward = 0.0
     
     def _q_loss(self, minibatch):
         # Compute loss                
@@ -147,6 +155,21 @@ class DDQN:
             L += (self.policy(s)[a] - y)**2
         L /= len(minibatch)
         return L
+    
+    def _update_network_weights(self, count):
+        if len(self._memory) > self.start_learn: 
+            # LEARN PHASE
+            self.optimizer.zero_grad()
+            minibatch = random.sample(self._memory, self.batch_size)
+            
+            loss = self._q_loss(minibatch)
+            loss.backward()
+            self.optimizer.step()
+            
+            # Update target network
+            if count % self.network_update_frequency == 0:
+                self._update_target_policy()
+        
         
         
         
--- train.py
+++ train.py
@@ -13,6 +13,12 @@ import numpy as np
 import random
 import torch_geometric
 
+
+import os
+import pickle
+import wandb
+import argparse
+
 def set_seed(seed):
     torch_geometric.seed_everything(seed)
     torch.manual_seed(seed)
@@ -22,9 +28,71 @@ def set_seed(seed):
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
+class EpochCallback:
+    def __init__(self, checkpoint_dir):
+        self.metrics = {'epoch_reward': []}  # By default, an epoch is 100 episodes
+        self.constant_metrics = {'epoch_number': -1, 
+                                 'max_epoch_reward': -99999999}
+        self.checkpoint_dir = checkpoint_dir
+    
+    def __call__(self, epoch_reward, epoch_count, model):
+        self.metrics['epoch_reward'].append(epoch_reward)
+        wandb.log({'epoch_reward': self.metrics['epoch_reward'][-1]})
+        
+        if epoch_reward > self.constant_metrics['min_epoch_reward']:
+            self.constant_metrics['epoch_number'] = epoch_count
+            self.constant_metrics['max_epoch_reward'] = epoch_reward
+            torch.save(model.state_dict(), checkpoint_dir + "reward_best.pth")
+            
+    
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--tag', type=str, default="dqn_gcn",
+                        help="Model name.")
+    parser.add_argument('--group_name', type=str, default=None,
+                        help="Specify an experiment group name to group together multiple runs")
+    parser.add_argument('--log', action="store_true", default=False,
+                        help="Log training curves") 
+    parser.add_argument('--seed', type=int, default=0,
+                        help="Seed for random initialisation.")             
+    
+    # Training hyperparameters
+    parser.add_argument('--batch_size', type=int, default=16, 
+                        help="Minibatch Size.")
+    parser.add_argument('--gamma', type=float, default=0.99,
+                        help="discount factor.")
+    parser.add_argument('--learning_rate', type=float, default=5.0e-04,
+                        help="learning rate for optimizer.")
+    parser.add_argument('--total_timesteps', type=int, default=100_000,
+                        help="The total of number of samples to train on.")
+    args = parser.parse_args()
+    return args
+
+def wandb_init():
+    wandb.init(project="GCN-DRL", group=args.group_name, job_type="train")
+    wandb.config.update(args)
+    
+def create_checkpoint_directory():
+    if not os.path.isdir("./checkpoint/"):
+        os.mkdir("./checkpoint/")
+    for i in range(1000):
+        checkpoint_dir = f"./checkpoint/{args.tag}_{i:03d}/"
+        if not os.path.isdir(checkpoint_dir):
+            os.mkdir(checkpoint_dir)
+        return checkpoint_dir
+    raise Exception("Unable to create checkpoint directory.")
+    
+def save_args(checkpoint_dir, args): 
+    with open(checkpoint_dir+'args.pkl', 'wb') as fp:
+        pickle.dump(args, fp)
+    
+        
 
 if __name__ == "__main__":
-    set_seed(0)
+    args = parse()
+    wandb_init()
+    
+    set_seed(args.seed)
     num_node_features = 4
     action_size = 3
         
@@ -39,7 +107,35 @@ if __name__ == "__main__":
                   discrete_actions=True)
     world.load_scene("scene01")
     
+    print('*'*30)
+    print("Training initiating....")
+    print(args)
+    
+    
     agent = GCNPolicy(num_node_features, action_size)
-    model = DDQN(world, agent)
-    model.learn(N_iter=100_000, log=True)
+    model = DDQN(world, 
+                 agent, 
+                 learning_rate=args.learning_rate,
+                 gamma=args.gamma,
+                 batch_size=args.batch_size,
+                 episodes_per_epoch=100)
+    
+    wandb.watch(agent, log="all", log_freq=100)
+
+    checkpoint_dir = create_checkpoint_directory()
+    print(f'Saving data to: {checkpoint_dir}')
+    
+    
+    save_args(checkpoint_dir, args)
+    
+    epoch_callback = EpochCallback(checkpoint_dir)
+    model.learn(total_timesteps=args.total_timesteps, 
+                log=args.log, 
+                epoch_callback=epoch_callback)
+    
+    
+    with open(checkpoint_dir+'metrics.pkl', 'wb') as fp:
+        pickle.dump(epoch_callback.metrics, fp)
+        
+    world.close()
     
